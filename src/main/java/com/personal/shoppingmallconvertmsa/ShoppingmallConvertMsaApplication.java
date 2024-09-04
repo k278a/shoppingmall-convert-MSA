dc52d97fad502b2764f879802f525eb4d290072a
USER K <ksar1030k@gmail.com>
2024-09-04 23:34:22 +0900

    first commit

---
+++ src/main/java/com/personal/shoppingmallconvertmsa/ShoppingmallConvertMsaApplication.java
@@ -0,0 +1,13 @@
+package com.personal.shoppingmallconvertmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallConvertMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmallConvertMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personal/shoppingmallconvertmsa/ShoppingmallConvertMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.shoppingmallconvertmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallConvertMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
